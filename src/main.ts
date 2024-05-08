1017b816d2af6968b7e2bf130e94826c1262ea93
Ahmed Fraz ud din <frazghuman@gmail.com>
2024-05-08 13:42:45 +0500

    updated

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ const app = document.getElementById('app');
 
 // Add an image to be displayed full-screen
 const img = document.createElement('img');
-img.src = '../assets/nyxia.png'; // Adjust path if necessary
+img.src = '../public/nyxia.png'; // Adjust path if necessary
 img.alt = 'Nyxia Ladies Planet';
 img.className = 'fullscreen';
 
